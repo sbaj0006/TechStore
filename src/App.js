70431de045556edfafa700be57ac6c0b082b093f
Smarth Bajaj <sbaj0006@student.monash.edu>
2020-03-09 17:19:45 +1100

    Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <h1>Hello from tech store</h1>;
+}
+
+export default App;
